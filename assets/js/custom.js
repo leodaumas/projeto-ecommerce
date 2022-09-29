22c50601fe7e8af072362f54ec79af9e4607739c
Jair Deon <jair.deon@gmail.com>
2022-09-29 08:05:09 -0300

    Atualização da página inicial e o início do responsivo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,7 +17,16 @@ $('.vantagens').owlCarousel({
     dots:true,
     margin: 10,
     responsive:{
-        1000:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
             items:4
         }
     }
@@ -33,12 +42,12 @@ $('.destaque-venda').owlCarousel({
     margin: 20,
     responsive:{
         0:{
-            items:1
-        },
-        600:{
             items:2
         },
-        1000:{
+        992:{
+            items:4
+        },
+        1200:{
             items:5
         }
     }
